5e8dddae1bc09cb37a3637272db82f4100d63ff3
ta631 <tatuo_ueda@kawachi.zaq.ne.jp>
2024-06-05 15:46:26 +0900

    6/5

--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,13 @@
 from django.views import generic
 
+from .forms import InquiryForm
+
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    form_class = InquiryForm
+    
