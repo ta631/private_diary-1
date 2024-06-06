5a2e50d0241be8e6fd4e3f0ad961aeca2b3a9b8e
ta631 <tatuo_ueda@kawachi.zaq.ne.jp>
2024-06-06 17:26:43 +0900

    6/6

--- diary/forms.py
+++ diary/forms.py
@@ -10,4 +10,14 @@ class InquiryForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        
+        self.fields['name'].widget.attrs['class'] = 'form-control'
+        self.fields['name'].widget.attrs['placeholder'] = 'お名前をここに入力してください。'
+
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['placeholder'] = 'メールアドレスをここに入力してください。'
+
+        self.fields['title'].widget.attrs['class'] = 'form-control'
+        self.fields['title'].widget.attrs['placeholder'] = 'タイトルをここに入力してください。'
+
+        self.fields['message'].widget.attrs['class'] = 'form-control'
+        self.fields['message'].widget.attrs['placeholder'] = 'メッセージをここに入力してください。'
